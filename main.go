0eaa4c37937b4491225ee7d085f3a67daa457630
Sohel Ahmed <sskm593@gmail.com>
2024-06-29 11:20:36 +0530

    added comments

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"math/rand"
 	"net/http"
 	"text/template"
+
 	"github.com/spf13/viper"
 	"gopkg.in/gomail.v2"
 )
 
+// Idata represents the structure of the JSON data from the API response
 type Idata struct {
 	Id    float32 `json:"id"`
 	Vn    float32 `json:"verse_number"`
@@ -35,6 +37,7 @@ type Idata struct {
 	} `json:"commentaries"`
 }
 
+// sendGoMail sends an email using the provided template and data to a list of recipients
 func sendGoMail(templatePath string, data interface{}, to []string) error {
 	// Parse the email template
 	t, err := template.ParseFiles(templatePath)
@@ -42,7 +45,7 @@ func sendGoMail(templatePath string, data interface{}, to []string) error {
 		return err
 	}
 
-	// Load the configuration
+	// Load the configuration from the YAML file
 	vi := viper.New()
 	vi.SetConfigFile("test.yaml")
 	err = vi.ReadInConfig()
@@ -50,7 +53,7 @@ func sendGoMail(templatePath string, data interface{}, to []string) error {
 		return err
 	}
 
-	// Create a new dialer
+	// Create a new SMTP dialer
 	d := gomail.NewDialer("smtp.gmail.com", 587, vi.GetString("mymail"), vi.GetString("mymailpassword"))
 
 	// Send email to each recipient individually
@@ -67,6 +70,7 @@ func sendGoMail(templatePath string, data interface{}, to []string) error {
 		m.SetHeader("Subject", "Gita Verse")
 		m.SetBody("text/html", body.String())
 
+		// Dial and send the email
 		err = d.DialAndSend(m)
 		if err != nil {
 			return err
@@ -77,20 +81,24 @@ func sendGoMail(templatePath string, data interface{}, to []string) error {
 }
 
 func main() {
-
+	// Generate random chapter and verse numbers
 	chapter := rand.Intn(18-1) + 1
 	verse := rand.Intn(20-1) + 1
 	url := fmt.Sprintf("https://bhagavad-gita3.p.rapidapi.com/v2/chapters/%d/verses/%d/", chapter, verse)
 
+	// Create a new HTTP request
 	req, _ := http.NewRequest("GET", url, nil)
 
+	// Load the configuration from the YAML file
 	vi := viper.New()
 	vi.SetConfigFile("test.yaml")
 	vi.ReadInConfig()
 
+	// Add API key and host to the request headers
 	req.Header.Add("X-RapidAPI-Key", vi.GetString("myapikey"))
 	req.Header.Add("X-RapidAPI-Host", "bhagavad-gita3.p.rapidapi.com")
 
+	// Send the HTTP request and get the response
 	res, _ := http.DefaultClient.Do(req)
 	if res.StatusCode >= 400 {
 		log.Println("Request failed with status:", res.StatusCode)
@@ -99,7 +107,7 @@ func main() {
 	defer res.Body.Close()
 	body, _ := io.ReadAll(res.Body)
 
-
+	// Parse the JSON response into the Idata struct
 	var idata Idata
 	err := json.Unmarshal(body, &idata)
 	if err != nil {
@@ -107,38 +115,47 @@ func main() {
 		return
 	}
 
+	// Print the raw text of the verse
 	rawText := idata.Text
 	fmt.Printf("%s\n", rawText)
+
+	// Extract the English translation if available
 	var trans string
-	if idata.Trans[0].Lang == "english" {
+	if len(idata.Trans) > 0 && idata.Trans[0].Lang == "english" {
 		trans = idata.Trans[0].Des
 		fmt.Printf("Meaning:\n %+v\n", trans)
 	}
 
+	// Prepare data for email template
 	data := struct {
-		Verse    string
+		Verse   string
 		Meaning string
 	}{
 		Verse:   idata.Text,
 		Meaning: trans,
 	}
 
+	// Create a new client for interacting with Airtable
 	client, _ := New(vi.GetString("airtabletoken"), vi.GetString("baseid"))
 
+	// Define the structure for Airtable records
 	type task struct {
 		AirtableID string
 		Fields     struct {
-			Name  string
-			Email string
+			Name       string
+			Email      string
 			Interested string
-			Date  string
+			Date       string
 		}
 	}
+
+	// Fetch the records from Airtable
 	tasks := []task{}
 	if err := client.ListRecords("mail", &tasks); err != nil {
 		panic(err)
 	}
 
+	// Collect emails of interested recipients
 	var emails []string
 	for _, t := range tasks {
 		if t.Fields.Interested == "Yes" {
@@ -148,6 +165,7 @@ func main() {
 
 	fmt.Println("All emails:", emails)
 
+	// Send the email to all interested recipients
 	err = sendGoMail("./template.html", data, emails)
 	if err != nil {
 		fmt.Println("Error sending email:", err)
--- pytable/textmail.go
+++ pytable/textmail.go
@@ -8,22 +8,27 @@ import (
 
 func main() {
 
+	// Open the file containing email addresses
 	emailsFile, err := os.Open("emails.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
 	}
+	// Ensure the file is closed when the function exits
 	defer emailsFile.Close()
+
+	// Slice to store the email addresses
 	to := []string{}
+	// Create a new scanner to read the file line by line
 	scanner := bufio.NewScanner(emailsFile)
 	for scanner.Scan() {
 		to = append(to, scanner.Text())
 	}
 
+	// Check for any errors encountered while reading the file
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
 	fmt.Println("All emails:", to)
 }
-
